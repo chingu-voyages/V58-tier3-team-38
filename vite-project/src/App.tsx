0d6fc876ac2fb89f303ec7b474f417453d9eef8b
MohaD <DMRgit@outlook.fr>
2025-12-09 19:31:59 +0100

    add the header on all page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import MapView from "./components/MapView.tsx";
+import MapView from "./components/MapView";
 import Home from "./components/Home";
-import HandleView from "./components/HandleView.tsx";
-import TableView from "./components/TableView.tsx";
-import Footer from "./components/Footer.tsx";
+import TableView from "./components/TableView";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/mapview"
-          element={
-            <>
-              <HandleView />
-              <MapView />
-            </>
-          }
-        />
-        <Route
-          path="/listview"
-          element={
-            <>
-              <HandleView />
-              <TableView />
-            </>
-          }
-        />
-      </Routes>
-      <Footer></Footer>
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <div className="flex-1 overflow-auto w-full flex justify-center pt-[390px]">
+        <div className="w-full max-w-7xl px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mapview" element={<MapView />} />
+            <Route path="/listview" element={<TableView />} />
+          </Routes>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
